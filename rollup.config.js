84309de1f3d70fe37c29e920bb192d30656ef113
Qin Xiaoqiang <qxq111@gmail.com>
2019-03-14 22:15:35 +0800

    add rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,18 @@
-import typescript from 'rollup-plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 
 export default {
-  input: './src/main.ts',
-  output: [
-    {file}
-  ],
+  input: 'src/main.ts',
+  output: [{
+    name: 'sdk',
+    file: 'dist/sdk.js',
+    format: 'umd',
+    sourcemap: true
+  }, {
+    name: 'sdk',
+    file: 'dist/sdk.es.js',
+    format: 'es',
+    sourcemap: true
+  }],
   plugins: [
     typescript()
   ]
